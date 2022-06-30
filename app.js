0a5443488c572be0132d9fdb6061708c8ca7f5fa
ANUAR_ARSTANBEKOV <kaliroseswiththegoldengun@gmail.com>
2022-06-30 17:52:46 +0600

    Slider without crashing

--- app.js
+++ app.js
@@ -1,8 +1,48 @@
-const sliderItems = document.querySelector(".slider__items");
-const navbarItems = document.querySelectorAll(".navbar__item");
+const sliderItem = document.querySelectorAll(".slider__item");
+const dot = document.querySelectorAll(".dot");
+let counter = 1;
+slide(counter);
 
-navbarItems.forEach((item, index) => {
-    item.addEventListener("click", () => {
-        sliderItems.style.transform = `translateX(${-100 * index}vw)`;
-    });
-});
+let timer = setInterval(autoSlide, 8000);
+function autoSlide() {
+    counter += 1;
+    slide(counter);
+}
+
+function plusSlides(n) {
+    counter += n;
+    slide(counter);
+    resetTimer();
+}
+
+function currentSlide(n) {
+    counter = n;
+    slide(counter);
+    resetTimer();
+}
+
+function resetTimer() {
+    clearInterval(timer);
+    timer = setInterval(autoSlide, 8000);
+}
+
+function slide(n) {
+    for(let i = 0; i < sliderItem.length; i++) {
+        sliderItem[i].style.display = "none";
+    }
+
+    for(let i = 0; i < dot.length; i++) {
+        dot[i].className = dot[i].className.replace(' active', '');
+    }
+
+    if(n > sliderItem.length) {
+        counter = 1;
+    }
+
+    if(n < 1) {
+        counter = sliderItem.length;
+    }
+
+    sliderItem[counter - 1].style.display = "block";
+    dot[counter - 1].className += " active";
+}
